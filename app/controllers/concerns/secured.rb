f08b56d6d66b2b47b818165fc638be3eeb123764
KlimSemikin <klimsemikin@gmail.com>
2022-02-16 19:48:37 +0600

    add default auth0 settings

---
+++ app/controllers/concerns/secured.rb
@@ -0,0 +1,27 @@
+module Secured
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :authenticate_request!
+  end
+
+  private
+
+  def authenticate_request!
+    @auth_payload, @auth_header = auth_token
+
+    render json: { errors: ['Insufficient scope'] }, status: :forbidden unless scope_included
+  rescue JWT::VerificationError, JWT::DecodeError
+    render json: { errors: ['Not Authenticated'] }, status: :unauthorized
+  end
+
+  def http_token
+    if request.headers['Authorization'].present?
+      request.headers['Authorization'].split(' ').last
+    end
+  end
+
+  def auth_token
+    JsonWebToken.verify(http_token)
+  end
+end
