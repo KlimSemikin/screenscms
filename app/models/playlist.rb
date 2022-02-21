2ee429aab71a846bce0917093ed164a671860c69
KlimSemikin <klimsemikin@gmail.com>
2022-02-21 14:26:11 +0600

    add validation to playlist

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -5,4 +5,5 @@ class Playlist < ApplicationRecord
   before_destroy { contents.clear }
 
   validates :name, presence: true, length: { maximum: 255 }, allow_blank: false
+  validates :duration, presence: true, numericality: { less_than_or_equal_to: 10000, only_integer: true }
 end
