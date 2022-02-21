cf1756104bb6e968276e93792151e828df3025b2
KlimSemikin <klimsemikin@gmail.com>
2022-02-21 18:16:19 +0600

    routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :contents
       resources :events do
         resources :screens do
-          resource :playlist
+          resource :playlist do
+            resources :contents
+          end
         end
       end
     end
