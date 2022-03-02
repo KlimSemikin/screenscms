9d4627b2abdb1e572a212273b6e9f3b5d151bc73
KlimSemikin <klimsemikin@gmail.com>
2022-03-02 13:41:18 +0600

    add routes move_lower, higher

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
       resources :events do
         resources :screens do
           resource :playlist, except: [:index, :create] do
-            resources :contents
+            resources :contents do
+              member do
+                post '/move_higher', to: 'contents#move_higher'
+                post '/move_lower', to: 'contents#move_lower'
+              end
+            end
           end
         end
       end
